80338ffa869d843318e0c2f5be9021e6e6c92180
KuyaMoEd <31286911+dipaaapi@users.noreply.github.com>
2022-03-17 23:56:48 +0800

    initial commit

---
+++ src/components/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <p class="">Hello Edward</p>
+    <p class="">welcome to the new project please show everything what you've got</p>
+</template>
+
+<style>
+    p {
+        color: red;
+    }
+</style>
